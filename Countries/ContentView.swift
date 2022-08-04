48b8155c8b04ac63a1cfb521018b341da2e1f10d
Sena Kurtak <zskurtak@gmail.com>
2022-08-05 00:51:21 +0300

    Add files via upload

---
+++ Countries/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countries
+//
+//  Created by Sena Kurtak on 5.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Sena Kurtak on 5.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
