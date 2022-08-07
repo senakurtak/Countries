da87e3a32c1264f9e07f90edae38d09a659db45a
senakurtak <zskurtak@gmail.com>
2022-08-07 16:10:01 +0300

    DetailCard designed.

--- Countries/Views/Screens/DetailCard/DetailCard.swift
+++ Countries/Views/Screens/DetailCard/DetailCard.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 import Alamofire
 struct DetailCard: View {
+    
+    @ Environment (\.openURL) private  var openURL
+    let url = URL(string: "https://www.wikidata.org/wiki/Q30")
+    @State var favoriteCheck = false
+    @State var color = Color.black.opacity(0.7)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            HStack{
+                Text("Turkey")
+                    .foregroundColor(.black.opacity(0.8))
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .frame(width: 350 , height: 50)
+                Button(action:{
+                    self.favoriteCheck.toggle()
+                }){ Image(systemName: self.favoriteCheck ? "star" : "star.fill").foregroundColor(self.color)}
+                Spacer()
+            }
+            
+            Image("Turkey")
+                .resizable()
+                .frame(width: 400, height: 240)
+                .padding(50)
+            HStack{
+                Text("Country Code:")
+                    .font(.body.bold())
+                    .foregroundColor(Color.black.opacity(0.8))
+                Text("TR")
+                    .font(.body.bold())
+                    .foregroundColor(Color.black.opacity(0.8))
+                Spacer()
+            }
+            .padding(20)
+            HStack {
+                Text("For More Information")
+                    .font(.body.bold())
+                    .padding(15)
+                    .background(Color(red: (65/230), green: 75/230, blue: 180/230))
+                    .foregroundColor(.white.opacity(0.70))
+                    .onTapGesture {
+                        openURL(url!)
+                    }
+                Spacer()
+            }
+            .padding(20)
+            Spacer()
+
+        }
+        
+        
+        
     }
+    
 }
 
 struct DetailCard_Previews: PreviewProvider {
--- Countries/Views/Screens/HomePage/HomePage.swift
+++ Countries/Views/Screens/HomePage/HomePage.swift
@@ -12,34 +12,30 @@ struct HomePage: View {
     @State var CountriesData : [Data] = []
     var body: some View {
         NavigationView{
-        VStack{
-            HStack{
-                Text("Countries")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black.opacity(0.8))
-            }
-            HStack{
-                NavigationLink(destination: DetailCard()){
-                    //            ForEach(CountriesData, id:\.self){item in
-                    //                Text(item.name)
-                    Text("Country from URL")
-                        .font(.body)
-                        .foregroundColor(.black.opacity(0.7))
-                        .frame(width: 320, height: 50)
-                        .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 1.2))
-                    
+            VStack{
+                HStack{
+                    Text("Countries")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black.opacity(0.8))
+                }
+                HStack{
+                    NavigationLink(destination: DetailCard()){
+                        //            ForEach(CountriesData, id:\.self){item in
+                        //                Text(item.name)
+                        Text("Country from URL")
+                            .font(.body)
+                            .foregroundColor(.black.opacity(0.7))
+                            .frame(width: 320, height: 50)
+                            .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 1.2))
+                    }
                 }
             }
         }
-        }
-        //
-        //
         //            }
         //        }.onAppear(){
         //            let countryRepo = CountryRepository()
         //            countryRepo.getCountries(){
         //                data in CountriesData = data
-        
     }
 }
