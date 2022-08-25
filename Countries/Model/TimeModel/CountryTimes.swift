6d4523526b030e5cde207c1e62c8e70c2965be9a
senakurtak <zskurtak@gmail.com>
2022-08-26 02:20:45 +0300

    countryTimes model and getTime function were added.

---
+++ Countries/Model/TimeModel/CountryTimes.swift
@@ -0,0 +1,13 @@
+//
+//  CountryTimes.swift
+//  Countries
+//
+//  Created by Sena Kurtak on 26.08.2022.
+//
+
+import Foundation
+
+struct CountryTimeModel: Codable {
+    var capital: String = ""
+    var timezones: [String] = [""]
+}
--- Countries/Service/NetworkService.swift
+++ Countries/Service/NetworkService.swift
@@ -11,42 +11,24 @@ import Alamofire
 class CountryRepository {
     func getCountries(completion:@escaping (CountriesDataModel) -> Void){
         
-        let headers: HTTPHeaders = [
-            "X-RapidAPI-Key": "cb98f696afmsh91bde0afd3313cbp109df6jsn9c1d2cfe5f6f",
-            
-        ]
-        
-        
+        let headers: HTTPHeaders = ["X-RapidAPI-Key": "cb98f696afmsh91bde0afd3313cbp109df6jsn9c1d2cfe5f6f"]
         let request = AF.request("https://wft-geo-db.p.rapidapi.com/v1/geo/countries", headers: headers)
-        
-        
-        
         request.responseDecodable(of: CountriesDataModel.self){response in
-            
             completion(response.value ?? CountriesDataModel())
-            
         }
     }
     
     func getCountryDetail(code:String, completion:@escaping (CountryDetailModel) -> Void){
-        
-        let headers: HTTPHeaders = [
-            "X-RapidAPI-Key": "cb98f696afmsh91bde0afd3313cbp109df6jsn9c1d2cfe5f6f",
-            
-        ]
-        
-        
+        let headers: HTTPHeaders = ["X-RapidAPI-Key": "cb98f696afmsh91bde0afd3313cbp109df6jsn9c1d2cfe5f6f"]
         let request = AF.request("https://wft-geo-db.p.rapidapi.com/v1/geo/countries/\(code)", headers: headers)
-        
-        
-        
         request.responseDecodable(of: CountryDetailModel.self){data in
-            
             completion(data.value ?? CountryDetailModel())
-        
         }
-        
-        
-        
+    }
+    func getTime(code:String, completion:@escaping (CountryTimeModel) -> Void){
+        let request = AF.request("https://restcountries.com/v2/alpha/\(code)")
+        request.responseDecodable(of: CountryTimeModel.self){data in
+            completion(data.value ?? CountryTimeModel())
+        }
     }
 }
